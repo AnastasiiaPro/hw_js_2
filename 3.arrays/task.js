0f13db0715cf36dabfea1c55a3102bca924fce6c
Anastasiia Prokhorova <naumtasya@gmail.com>
2024-01-31 19:48:20 +0700

    Added arrays file

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,9 @@
 function compareArrays(arr1, arr2) {
-  
+	return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	let us = users.filter(item => item.gender === gender).map(item => item.age);
+	let res = us.reduce((sum, age) => sum + age, 0);
+	return (us.length > 0) ? res / us.length : 0;
 }
